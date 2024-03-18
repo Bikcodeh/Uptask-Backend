882408027fc0182636822a2b4f4c99639801eb1b
Bikcodeh <bikcodeh@gmail.com>
2024-03-18 18:31:52 -0500

    little refactor for inversify container

--- src/features/projects/infraestructure/routes/projectRoutes.ts
+++ src/features/projects/infraestructure/routes/projectRoutes.ts
@@ -1,6 +1,6 @@
 import { ProjectController } from '../controller/ProjectController';
 import { Router } from "express";
-import { container } from "../../../../config/di";
+import container from "../../../../config/di";
 
 const router = Router();
 
